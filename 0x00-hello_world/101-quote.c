2e26981d93543707c3510cc8556b23381786c08b
Meki22 <meki3422@gmail.com>
2022-06-30 12:45:13 -0700

    advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,27 +1,17 @@
 #include <stdio.h>
 
-#include <unistd.h>
-
-/**
+/*
  *
- *  * main - prints exactly *and that piece of art is useful*
+ * * main - Entry Point
  *
- *   * -Dora korpar,2015-10-19*,
+ * *
  *
- *    *followed by a new line,to the standard error.
+ * * Description: 'advanced '
  *
- *     * Return: always 0 (success)
+ * *
  *
- *      */
-
-int main(void)
-
-{
-
-	write(2, "and that piece of art is useful\" .Dora korpar,2015-10-19\n*,59);
-
-	return (1);
-
-}
+ * * Return: Always 0 (Success)
+ *
+ * */
 
 
